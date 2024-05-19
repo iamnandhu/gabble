ea05726926b9cf12e0930ab8cc5a38b4e7e5f44e
iamnandhu <nandhus@Admins-MacBook-Air.local>
2024-05-20 00:33:09 +0530

    feat: setup register user with loading animation

--- src/dto/dto.ts
+++ src/dto/dto.ts
@@ -1,3 +1,6 @@
+import { User, UserCredential, UserInfo, UserMetadata } from "firebase/auth";
+
+
 export interface firebaseConfig {
     apiKey: string,
     authDomain: string,
@@ -6,4 +9,39 @@ export interface firebaseConfig {
     messagingSenderId: string,
     appId: string,
     measurementId: string,
+}
+
+export interface signUp {
+    userName: string,
+    email: string,
+    password: string
+}
+
+export interface providerData extends UserInfo {
+    uid: string;
+    displayName: string | null;
+    email: string | null;
+    phoneNumber: string | null;
+    photoURL: string | null;
+    providerId: string;
+}
+
+export interface userMetaData extends UserMetadata {
+    creationTime?: string;
+    lastSignInTime?: string;
+}
+
+export interface user extends User {
+    emailVerified: boolean;
+    isAnonymous: boolean;
+    metadata: userMetaData;
+    providerData: providerData[];
+    refreshToken: string;
+    tenantId: string | null;
+}
+
+export interface userSignUpResponseModel extends UserCredential {
+    user: User;
+    providerId: string | null;
+    operationType: "link" | "reauthenticate" | "signIn";
 }
